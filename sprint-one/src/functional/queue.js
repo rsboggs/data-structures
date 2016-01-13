c31a02c7cfca84b1d5186b953ad0d6c4def7eb6b
rsboggs <robertstephenboggs@gmail.com>
2016-01-13 15:05:39 -0800

    Implement FIFO

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,11 +8,17 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance[stackSize] = value;
     stackSize++;
   };
 
   someInstance.dequeue = function() {
-    stackSize--;
+    if (stackSize) {
+      var toBeDeleted = someInstance[0];
+      delete someInstance[0];
+      stackSize--;
+      return toBeDeleted;
+    }
   };
 
   someInstance.size = function() {
