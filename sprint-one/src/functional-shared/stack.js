82f375f6560d66f9861f4275f909726ea0003829
rsboggs <robertstephenboggs@gmail.com>
2016-01-13 15:31:56 -0800

    extended obj with stackMethods and return size

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,15 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {
+    stackSize: 0
+  };
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  size: function() {
+    return this.stackSize;
+  }
+};
