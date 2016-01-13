226a8eeecb2fd96d1f5d5165475324ada93487f9
rsboggs <robertstephenboggs@gmail.com>
2016-01-13 15:58:40 -0800

    Return stack size when calling with size method

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,15 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {stackSize: 0};
+  _.extend(obj, queueMethods);
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.stackSize;
+  }
+};
 
 
