82afa34d7b07ac7cb8c7c676648f201dd01ab0f3
rsboggs <robertstephenboggs@gmail.com>
2016-01-13 16:37:27 -0800

    Return size of the object

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,15 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.stackSize = 0;
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return this.stackSize;
+  }
+};
 
 
