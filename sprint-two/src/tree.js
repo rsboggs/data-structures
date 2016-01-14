d5a116af6912ccf50984d2c643b80adbb5b46892
rsboggs <robertstephenboggs@gmail.com>
2016-01-14 11:15:36 -0800

    extend newTree with treeMethods and implement addChild

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +12,11 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  
 };
 
 
