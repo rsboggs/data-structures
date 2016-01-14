9cdc8b7e282d1ed74bc033cb4903735774e45a66
rsboggs <robertstephenboggs@gmail.com>
2016-01-13 16:28:23 -0800

    return stackSize when calling size

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,14 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.stackSize = 0;
+  return obj;
 };
 
-var stackMethods = {};
-
+var stackMethods = {
+  size: function() {
+    return this.stackSize;
+  }
+};
 
