ea5a896b691a0046c3932cc70ee0ed3adc9a9290
rsboggs <robertstephenboggs@gmail.com>
2016-01-14 10:45:25 -0800

    Implement addToTail and removeHead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,25 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if(list.tail === null) {
+      var tail = list.tail = Node(value);
+      list.head = tail;
+    } else if(list.head === null) {
+      list.head = list.tail;
+      list.tail = Node(value);
+      list.head.next = list.tail;
+    } else {
+      var oldTail = list.tail;
+      var newTail = Node(value);
+      oldTail.next = newTail;
+      list.tail = newTail;
+    }
   };
 
   list.removeHead = function() {
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
   list.contains = function(target) {
@@ -26,4 +42,4 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+ */ 
