6d51c58a444462cc9adc9b974a31f4a87c61a3c4
rsboggs <robertstephenboggs@gmail.com>
2016-01-13 20:38:05 -0800

    Implement size and enqueue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,14 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
 };
 
+Queue.prototype.size = function() {
+  return this.stackSize;
+};
 
+Queue.prototype.enqueue = function(val) {
+  this[this.stackSize] = val;
+  this.stackSize++;
+};
