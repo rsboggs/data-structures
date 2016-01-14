d0e7aa6191c867921ab40c43c32cb573f81ac6a5
rsboggs <robertstephenboggs@gmail.com>
2016-01-13 20:33:20 -0800

    implement push and size

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,18 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
 };
+
+Stack.prototype.size = function() { 
+ return this.stackSize;
+};
+
+Stack.prototype.push = function(val) {
+  this[this.stackSize] = val;
+  this.stackSize++;
+};
+
+
 
 
