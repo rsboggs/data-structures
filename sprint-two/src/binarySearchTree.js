cf2eaf769105ec27d14b2b380f9217077f120555
rsboggs <robertstephenboggs@gmail.com>
2016-01-15 11:48:17 -0800

    Implement insert method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,14 +1,26 @@
 var BinarySearchTree = function(value) {
 
   var obj = Object.create(BinarySearchTree.prototype);
-  obj.val = value;
+  obj.value = value;
   obj.left = null;
   obj.right = null;
   return obj;
 };
 
 BinarySearchTree.prototype.insert = function(val) {
-
+  if (this.value > val) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(val);
+    } else {
+      this.left.insert(val);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = BinarySearchTree(val);
+    } else {
+      this.right.insert(val);
+    }
+  }
 };
 
 BinarySearchTree.prototype.contains = function(val) {
