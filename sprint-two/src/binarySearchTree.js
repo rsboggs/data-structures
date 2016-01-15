24a669dcd4ea14d02db993a0b7a9f28bb7c0881c
rsboggs <robertstephenboggs@gmail.com>
2016-01-15 11:37:06 -0800

    Create object and associated functions with object

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,23 @@
 var BinarySearchTree = function(value) {
+
+  var obj = Object.create(BinarySearchTree.prototype);
+  obj.val = value;
+  obj.left = null;
+  obj.right = null;
+  return obj;
 };
 
+BinarySearchTree.prototype.insert = function(val) {
+
+};
+
+BinarySearchTree.prototype.contains = function(val) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb, val) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
