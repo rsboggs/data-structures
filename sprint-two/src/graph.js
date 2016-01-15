14e17d623943d5773db3d245e155cc7ec42a6ee2
rsboggs <robertstephenboggs@gmail.com>
2016-01-14 21:20:24 -0800

    implement graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,41 +3,69 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this[node] = {
+    val: node,
+    edges: [] 
+  };
 };
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  for(var key in this) {
+    if(this[key].val === node) {
+      return true;
+    } 
+  }
+  return false;
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this[node];
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for(var i = 0; i < this[fromNode].edges.length; i++) {
+    if(this[fromNode].edges.indexOf(toNode) >= 0) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this[fromNode].edges.push(toNode);
+  this[toNode].edges.push(fromNode);
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  this[fromNode].edges.splice(this[fromNode].edges.indexOf(toNode),1);
+  this[toNode].edges.splice(this[toNode].edges.indexOf(fromNode),1);
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for(var key in this) {
+    if (this[key].constructor !== Function) {
+      console.log(key);
+      cb(this[key].val);
+    }
+  }
 };
 
 /*
