b9ef0f4977fb1fa3ab8b8dcfdd293527c7c7d74f
rsboggs <robertstephenboggs@gmail.com>
2016-01-15 11:24:45 -0800

    implement hash table with tuples and buckets. Pass for collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,21 +7,59 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, {k: v});
+  var bucket = [];
+  var tuple = [k,v];
+  //if !Array.isArray(this._storage.get(index))
+  if (!Array.isArray(this._storage.get(index))) {
+    //bucket.push tuple set the value of the index = bucket
+    bucket.push(tuple);
+  //else 
+  } else { 
+    //For loop through bucket
+    bucket = this._storage.get(index);
+    var found = false;
+    for (var i = 0; i < bucket.length; i++) {
+      //if keys are same
+      if (k === bucket[i][0]) {
+        //push the tuple into the bucket
+        bucket[i][1] = v;
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      bucket.push(tuple);
+    }
+  }
+  //at end set bucket
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index).k;
+  var temp = this._storage.get(index);
+  for(var i = 0; i < temp.length; i++) {
+    if(temp[i][0] === k) {
+      return temp[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each(function(item, ind, storage) {
-    if(index === ind) {
-      delete storage[ind].k;
+  // this._storage.each(function(item, ind, storage) {
+  //   if(index === ind) {
+  //     delete storage[ind].k;
+  //   }
+  // });
+  var temp = this._storage.get(index);
+  for (var i = 0; i < temp.length; i++) {
+    if (temp[i][0] === k) {
+      temp.splice(i, 1);
+      return true;
     }
-  });
+  }
+  this._storage.set(index, temp);
 };
 
 
